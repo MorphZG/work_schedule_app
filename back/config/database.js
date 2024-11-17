e4f181d91511eb034d6a57ef5c5b6dcf363e1dc4
MorphZG <zoran1607@gmail.com>
2024-11-17 03:48:32 +0100

    include database.js

---
+++ back/config/database.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import "dotenv/config";
+
+
+const connect_db = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.LOCALHOST, {
+            autoCreate: true,  // creates collection if it doesn't exist
+        });
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+    }
+};
+
+export { connect_db };
