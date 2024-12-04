26926925dfe2e48b956659c16a738d06a2ae067a
MorphZG <zoran1607@gmail.com>
2024-12-04 13:25:15 +0100

    refactor: add server.port option

--- frontend/vite-project/vite.config.js
+++ frontend/vite-project/vite.config.js
@@ -6,6 +6,9 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: 4000
+  },
   plugins: [react()],
   css: { postcss: { plugins: [tailwindcss()] } },
   resolve: {
